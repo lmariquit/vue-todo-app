1d97b896ad4be0079e26abd82f498ac43cdbe907
Loren Mariquit <l.mariquit@gmail.com>
2019-05-09 22:04:08 -0400

    moved header into app component and moved routeslinks into header component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>This is my first Vue application. I followed a vue crashcourse created by Traversy Media: https://www.youtube.com/watch?v=Wy9q22isx3U</p>
   </div>
 </template>
