246ac3eb50ca42d0638ca1c0f40b4a4e9848b678
Loren Mariquit <l.mariquit@gmail.com>
2019-05-09 21:58:15 -0400

    added vue router. had an issue that was fixed by putting plugin in main.js

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,12 @@
 <template>
   <div id="app">
-    <Header/>
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Header from './components/layout/Header'
-import Todos from './components/Todos'
-import AddTodo from './components/AddTodo'
-import axios from 'axios'
-
-export default {
-  name: 'app',
-  components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    async deleteTodo(id) {
-      try {
-        await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        this.todos = this.todos.filter(todo => todo.id !== id)
-      } catch (err) {
-        // eslint-disable-next-line
-        console.log('There has been an error with deleteTodo --> ', err)
-      }
-    },
-    async addTodo(newTodo) {
-      const { title, completed } = newTodo
-      try {
-        const res = await axios.post(
-          'https://jsonplaceholder.typicode.com/todos',
-          {
-            title,
-            completed
-          }
-        )
-        this.todos = [...this.todos, res.data]
-      } catch (err) {
-        // eslint-disable-next-line
-        console.log('There has been an error with addTodo --> ', err)
-      }
-    }
-  },
-  async created() {
-    try {
-      const res = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos?_limit=10'
-      )
-      this.todos = res.data
-    } catch (err) {
-      // eslint-disable-next-line
-      console.log('There has been an error with created --> ', err)
-    }
-  }
-}
-</script>
 
 <style>
 * {
@@ -71,6 +14,7 @@ export default {
   margin: 0;
   padding: 0;
 }
+
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
